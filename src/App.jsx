0e21e4c0995bd5f326f2463b334a55ba74c59120
Ramón Gavira <ramgavsan@alum.us.es>
2025-12-06 21:45:26 +0100

    feat: implement routes to visit profiles

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Feed from './pages/app/Feed';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Profile from './pages/app/profile/Profile';
+import ProfileView from './pages/app/profile/ProfileView';
 import './styles/App.css';
 import DashboardsPage from './pages/app/dashboards/DashboardsPage';
 import CreateDashboards from './pages/app/dashboards/CreateDashboards';
@@ -41,7 +42,8 @@ function App() {
           <Route path="upload" element={<Feed />} />
           <Route path="library" element={<Feed />} />
           <Route path="messages" element={<Feed />} />
-          <Route path="profile" element={<Profile />} />
+          <Route path="profile" element={<ProfileView />} />
+          <Route path="profile/:username" element={<ProfileView />} />
 
           {/* Rutas del microservicio Dashboards */}
           <Route path="/app/dashboards" element={<DashboardsPage />} />
