60ea8f59ef771c6e64b0f18af6ec7f0fb5128112
dangalcan <megamagolas@gmail.com>
2025-12-18 11:54:25 +0100

    feat: add prepath to api-routes in order to communicate properly with api-gateway

--- src/services/beats-interaction/playlistService.js
+++ src/services/beats-interaction/playlistService.js
@@ -1,37 +1,37 @@
 import { client } from '@/api/axiosClient';
 
 export function createPlaylist(data) {
-  return client.post('/playlists', data)
+  return client.post('/beats-interactions/playlists', data)
 }
 
 export function getMyPlaylists() {
-  return client.get('/playlists/me')
+  return client.get('/beats-interactions/playlists/me')
 }
 
 export function getUserPlaylists(userId) {
-  return client.get(`/playlists/user/${userId}`)
+  return client.get(`/beats-interactions/playlists/user/${userId}`)
 }
 
 export function getPublicPlaylists() {
-  return client.get('/playlists/public')
+  return client.get('/beats-interactions/playlists/public')
 }
 
 export function getPlaylistById(id) {
-  return client.get(`/playlists/${id}`)
+  return client.get(`/beats-interactions/playlists/${id}`)
 }
 
 export function updatePlaylist(id, data) {
-  return client.patch(`/playlists/${id}`, data)
+  return client.patch(`/beats-interactions/playlists/${id}`, data)
 }
 
 export function deletePlaylist(id) {
-  return client.delete(`/playlists/${id}`)
+  return client.delete(`/beats-interactions/playlists/${id}`)
 }
 
 export function addBeatToPlaylist(id, data) {
-  return client.post(`/playlists/${id}/items`, data)
+  return client.post(`/beats-interactions/playlists/${id}/items`, data)
 }
 
 export function removeBeatFromPlaylist(playlistId, beatId) {
-  return client.delete(`/playlists/${playlistId}/items/${beatId}`)
+  return client.delete(`/beats-interactions/playlists/${playlistId}/items/${beatId}`)
 }
